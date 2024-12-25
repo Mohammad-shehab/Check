6458eba8a276b36843e7efa3d97aff54807462c1
Mohammad-shehab <q8mlg@outlook.com>
2024-12-25 13:26:54 +0300

    Add project files.

---
+++ taskcheck/Models/Salary.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace taskcheck.Models
+{
+    internal class Salary
+    {
+        private double Salary1;
+        private string User_Name1;
+
+
+
+        public string user_name
+        {
+            get { return User_Name1; }
+            set
+            {
+                if (value == null || value ==string.Empty)
+                {
+                    Console.WriteLine("Invalid UserName");
+                    
+                }
+                else
+                {
+                    User_Name1 = value;
+                }
+            }
+        }
+
+        public double salary
+        {
+            get { return Salary1; }
+            set
+            {
+                if (value >= 350 && value <= 900)
+                {
+                    Salary1 = value;
+                }
+                else
+                {
+                    Console.WriteLine("out of range");
+                }
+            }
+        }
+
+
+
+
+
+
+
+        //public void CheckUserName()
+        //{
+        //    if (User_Name1 != null)
+        //    {
+        //        Console.WriteLine("my UserName is " + User_Name1);
+        //    }
+
+        //}
+
+        //public void CheckSalary()
+        //{
+        //    if (Salary1== 0)
+        //    {
+        //        Console.WriteLine("not in range " );
+        //    }
+        //    else
+        //    {
+        //        Console.WriteLine("my salary is " + Salary1);
+        //    }
+
+        //}
+
+
+
+    }
+}
---
+++ taskcheck/Program.cs
@@ -0,0 +1,17 @@
+﻿// enter username 
+
+
+using taskcheck.Models;
+
+Console.WriteLine("Enter your username");
+string username = Console.ReadLine();
+
+Salary salary = new Salary();
+
+
+salary.user_name = username;
+
+Console.WriteLine("Enter your salary");
+double salary1 = Convert.ToDouble(Console.ReadLine());
+salary.salary = salary1;
+//salary.CheckSalary();
